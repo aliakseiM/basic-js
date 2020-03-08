ff9e0b6f17de326b218041fa27459fc4ba4129d1
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2020-03-08 18:04:54 +0300

    feat: complete 'what-season'

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,15 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date===undefined) {
+    return 'Unable to determine the time of year!';
+  } else if (date.toString() === Date()) {
+    throw Error;
+  }
+  let monthIdx = date.getMonth();
+  switch (true) {
+    case (monthIdx >= 0 && (monthIdx < 2 || monthIdx === 11)) : return "winter";
+    case (monthIdx >= 2 && monthIdx < 5) : return "spring";
+    case (monthIdx >= 5 && monthIdx < 8) : return "summer";
+    case (monthIdx >= 8 && monthIdx < 11) : return "autumn";
+    default: return 'Unable to determine the time of year!';
+  }
 };
